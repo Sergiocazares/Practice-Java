e6536e04a0fe486e65a07e43d55732739f87ceac
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2021-06-15 22:26:25 -0500

    primitive data types

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,26 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int number = 100;
-        String brand = "Sergiocode";
+        //Allows store whole numbers |underscore helps visualize the number|
+        byte theByte = -128;
+        short theShort = 32_000;
+        int theInt = 787_878_334;
+        long theLong = 7_878_783_342L;
+        //Store Decimals
+        float pi = 3.14F;
+        double doublePi = 3.1415;
+        //True or False
+        boolean isAdult = false;
+        //Store single characters
+        char nameInitial = 'A';
+
+        System.out.println(theByte);
+        System.out.println(theShort);
+        System.out.println(theLong);
+        System.out.println(theInt);
+        System.out.println(pi);
+        System.out.println(doublePi);
+        System.out.println(isAdult);
+        System.out.println(nameInitial);
     }
 }
