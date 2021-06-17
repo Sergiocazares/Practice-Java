956a7e061f50ec75d6c0b1e5c403db9968314b9f
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2021-06-16 22:05:48 -0500

    packages and import keyword

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,14 @@
 package com.company;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 public class Main {
 
     public static void main(String[] args) {
-        //String (data type) | name (name) | new String (object)
-        String name = new String("Alex");
-        System.out.println(name.toUpperCase());
-        System.out.println(name.toLowerCase());
-        System.out.println(name.charAt(1));
-        String code = new String("lex");
-        System.out.println(name.contains("lex"));
+        Date date = new Date();
+        LocalDate localDate = LocalDate.now();
+        LocalDateTime localDateTime = LocalDateTime.now();
     }
 }
