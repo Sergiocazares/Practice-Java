057a6c9304c36f374752ab0258db7b3cd1be6060
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2021-06-16 21:21:11 -0500

    difference between primitives and reference types

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,10 +5,30 @@ import java.time.LocalDate;
 public class Main {
 
     public static void main(String[] args) {
-        //Non primitive data types AKA reference types
-        String name = new String("Sergiocode");
-        System.out.println(name.toUpperCase());
-        LocalDate now = LocalDate.now();
-        System.out.println(now.getMonth());
+        //int b is untouched lives in diff memory (b=10 , a=100)
+        int a = 10;
+        int b = a;
+        a = 100;
+        System.out.println("a = " + a  + " - b = '" +b);
+
+
+        //reference type , both reference to object person("alex")
+        Person alex = new Person("alex");
+        Person mariam = alex;
+
+        System.out.println("before changing alex");
+        System.out.println(alex.name + " " + mariam.name);
+
+        alex.name = "Alexander";
+
+        System.out.println("after changing alex");
+        System.out.println(alex.name + " " + mariam.name);
+    }
+
+    static class Person {
+        String name;
+        Person(String name) {
+            this.name = name;
+        }
     }
 }
