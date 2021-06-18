2ac6a87f4a64ce45ee4199d6b5fa8c47f429a929
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2021-06-17 22:05:56 -0500

    comparison operators

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        // Math
-        System.out.println(Math.abs(-10));
-        System.out.println(Math.max(6, 10));
-        System.out.println(Math.min(6, 10));
-        System.out.println(Math.pow(5.0, 2.0));
+        // Comparison Operators
+        int alexAge = 18;
+        int sergioAge = 20;
+        System.out.println(alexAge > sergioAge);
+        System.out.println(alexAge < sergioAge);
+        System.out.println(alexAge == sergioAge);
+        System.out.println(alexAge != sergioAge);
     }
 }
