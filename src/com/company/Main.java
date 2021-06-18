8440302f857d82a8e5c1b6ee38578950d2bfcfee
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2021-06-18 17:36:45 -0500

    Ternary Operator

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        // If statements
-        int age = 17;
-        if (age > 18) {
-            System.out.println("Yay ... I am an adult");
-        } else if (age >= 16 && age <18) {
-            System.out.println("I am almost an adult");
-        }
+        // Ternary Operator
+        int age = 18;
+        String message = age >= 18 ?
+                "Yay... I am an adult" : "I am not an adult";
+        System.out.println(message);
     }
 }
