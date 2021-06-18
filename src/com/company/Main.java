e58d6715c6432d1a242e0ae981ec07efb9c7ce1c
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2021-06-18 17:40:37 -0500

    Switch Statements

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,28 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        // Ternary Operator
-        int age = 18;
-        String message = age >= 18 ?
-                "Yay... I am an adult" : "I am not an adult";
-        System.out.println(message);
+        // Switch Statement
+        String gender = "da";
+        if (gender.equals("Male")) {
+
+        } else if (gender.equals("Prefer_Not_Say")) {
+
+        } else {
+
+        }
+
+        switch (gender.toUpperCase()){
+            case "Female":
+                System.out.println("I am a female");
+                break;
+            case "Male":
+                System.out.println("I am a male");
+                break;
+            case "Prefer_Not_Say":
+                System.out.println("Prefer not say");
+                break;
+            default:
+                System.out.println("Unknown Gender");
+        }
     }
 }
