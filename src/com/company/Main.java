e0c52ee23e964de34586a6be63672df7d5d3ba39
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2021-06-19 22:57:53 -0500

    enchanced loops

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,16 +5,15 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        // Loops
+        // Enhanced Loops
         int [] numbers = {2, 0, 1, 4, 100, 4, 90, 78, 77};
 
         for (int i = 0; i < numbers.length; i++) {
             System.out.println(numbers[i]);
         }
-
-        // Reverse Order
-        for (int i = numbers.length -1; i >= 0; i--) {
-            System.out.println(numbers[i]);
+        System.out.println("Enhanced for loop");
+        for (int number : numbers) {
+            System.out.println(number);
         }
     }
 }
