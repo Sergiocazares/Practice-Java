9e8164f3434c1383dbac8aad6116770037571263
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2021-06-20 21:51:03 -0500

    Break and Continue

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,15 +5,13 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        // Enhanced Loops
-        int [] numbers = {2, 0, 1, 4, 100, 4, 90, 78, 77};
-
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.println(numbers[i]);
-        }
-        System.out.println("Enhanced for loop");
-        for (int number : numbers) {
-            System.out.println(number);
+        // Break and Continue
+        String[] names = {"Anna", "Ali", "Bob", "Mike"};
+        for (String name : names) {
+            if (name.startsWith("A")) {
+                continue;
+            }
+            System.out.println(name);
         }
     }
 }
