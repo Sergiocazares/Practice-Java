2ad9157bf53ce56ea70508c832b6d96a6f103789
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2021-06-20 21:54:28 -0500

    Do While Loops

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,16 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        // While Loop
-        int count = 8;
+        // Do While Loop
+        int count = 0;
 
-        while (count <= 20) {
-            System.out.println("count " + count);
+        do {
+            System.out.println(count);
             count++;
+        }
+        while (count <= 20);
+        while (true) {
+
         }
     }
 }
