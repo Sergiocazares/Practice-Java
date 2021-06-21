0b8b27b5b03a55d16291c43198f0f942911d2072
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2021-06-21 13:18:38 -0500

    Methods

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,20 +1,13 @@
 package com.company;
 
-import java.time.LocalDateTime;
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-        // Scanner
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("What is your name?");
-        String userName = scanner.nextLine();
-        System.out.println("Hello " + userName);
+        // Methods
+        System.out.println("Hello");
+        String brand = "Samsung";
+        System.out.println(brand.toUpperCase());
+        System.out.println(brand.startsWith("s"));
 
-        System.out.println("How old are you?");
-        int age = scanner.nextInt();
-        int year = LocalDateTime.now().minusYears(age).getYear();
-        System.out.println("You were born in " + year);
     }
 }
