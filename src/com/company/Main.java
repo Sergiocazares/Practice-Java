e74523cdddbcb7fe0f7406f61e5a040c8440476b
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2021-06-22 21:37:13 -0500

    passport example

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,39 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        // Classes and Objects
-        Lens lensOne = new Lens( "sony", "85mm", true);
-        Lens lensTwo = new Lens( "sony", "30mm", true);
-        Lens lensThree = new Lens( "sony", "24-70mm", false);
-
-        System.out.println("Lens 1");
-        System.out.println(lensOne.brand);
-        System.out.println(lensOne.focalLength);
-        System.out.println(lensOne.isPrime);
-        System.out.println();
-
-        System.out.println("Lens 2");
-        System.out.println(lensTwo.brand);
-        System.out.println(lensTwo.focalLength);
-        System.out.println(lensTwo.isPrime);
-        System.out.println();
-
-        System.out.println("Lens 3");
-        System.out.println(lensThree.brand);
-        System.out.println(lensThree.focalLength);
-        System.out.println(lensThree.isPrime);
+        // Classes
+       String passportNumber = "12456";
     }
-
-    static class Lens {
-        String brand;
-        String focalLength;
-        boolean isPrime;
-
-        Lens(String brand, String focalLength, boolean isPrime) {
-            this.brand = brand;
-            this.focalLength = focalLength;
-            this.isPrime = isPrime;
-        }
-    }
-
 }
