26e66e45fe5e676191960f64ee402d4c97417244
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2021-06-22 21:32:56 -0500

    Creating Objects

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
         // Classes and Objects
+        Lens lensOne = new Lens( "sony", "85mm", true);
+        Lens lensTwo = new Lens( "sony", "30mm", true);
+        Lens lensThree = new Lens( "sony", "24-70mm", false);
+
     }
 
     static class Lens {
