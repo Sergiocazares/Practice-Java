9b0c3c599fba2ddda843450c10bb67001f2b7de5
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2021-06-22 21:35:41 -0500

    Printing Object Attributes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,6 +8,22 @@ public class Main {
         Lens lensTwo = new Lens( "sony", "30mm", true);
         Lens lensThree = new Lens( "sony", "24-70mm", false);
 
+        System.out.println("Lens 1");
+        System.out.println(lensOne.brand);
+        System.out.println(lensOne.focalLength);
+        System.out.println(lensOne.isPrime);
+        System.out.println();
+
+        System.out.println("Lens 2");
+        System.out.println(lensTwo.brand);
+        System.out.println(lensTwo.focalLength);
+        System.out.println(lensTwo.isPrime);
+        System.out.println();
+
+        System.out.println("Lens 3");
+        System.out.println(lensThree.brand);
+        System.out.println(lensThree.focalLength);
+        System.out.println(lensThree.isPrime);
     }
 
     static class Lens {
