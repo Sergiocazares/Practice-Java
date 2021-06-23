78ae4f8164217b0b637282a701257711c5c0d777
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2021-06-22 21:50:14 -0500

    Passport Done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,26 @@
 package com.company;
 
+import java.time.LocalDate;
+
 public class Main {
 
     public static void main(String[] args) {
         // Classes
-       String passportNumber = "12456";
+        Passport usPassport = new Passport("1234", "United States", LocalDate.of(2025, 1, 1));
+
+        Passport uKPassport = new Passport("1239", "England", LocalDate.of(2030, 1, 1));
+
+    }
+
+    static class Passport {
+        String number;
+        String country;
+        LocalDate expiredDate;
+
+        Passport(String number, String country, LocalDate expiredDate){
+            this.number = number;
+            this.country = country;
+            this.expiredDate = expiredDate;
+        }
     }
 }
