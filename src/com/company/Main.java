8e0b51edcada48305647f6e97aac3c16ea98bdfd
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2021-06-23 23:17:48 -0500

    Imperative Approach

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,26 +1,15 @@
 package com.company;
 
 import java.time.LocalDate;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        // Classes
-        Passport usPassport = new Passport("1234", "United States", LocalDate.of(2025, 1, 1));
+        // Imperative Approach
+        List<Person> people List.of(
 
-        Passport uKPassport = new Passport("1239", "England", LocalDate.of(2030, 1, 1));
-
+        );
     }
 
-    static class Passport {
-        String number;
-        String country;
-        LocalDate expiredDate;
-
-        Passport(String number, String country, LocalDate expiredDate){
-            this.number = number;
-            this.country = country;
-            this.expiredDate = expiredDate;
-        }
-    }
 }
