996bfc73dd9e9d922630f8c532a4e3a494eba63f
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2021-06-23 23:25:20 -0500

    Imperative Approach List

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,15 +1,39 @@
 package com.company;
 
-import java.time.LocalDate;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
         // Imperative Approach
-        List<Person> people List.of(
+        List<Person> people = List.of(
+                new Person("John", MALE),
+                new Person("Maria", FEMALE),
+                new Person("Aisha", MALE),
+                new Person("Alex", MALE),
+                new Person("Alice", FEMALE)
 
         );
+    }
+
+    static class Person {
+        private final String name;
+        private final Gender gender;
+
+        Person(String name, Gender gender){
+            this.name = name;
+            this.gender = gender;
+        }
+
+        @Override
+        public String toString(){
+            return "Person{" + "name='" + name + '\'' + ", gender=" + gender + '}';
+        }
+
+        enum Gender {
+            MALE, FEMALE
+        }
+
     }
 
 }
