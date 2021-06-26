f0204f74861708783b03d5e56eb18722de6c8213
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2021-06-25 19:29:47 -0500

    Array list

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,6 +14,7 @@ public class Main {
                 new Person("Alice", Person.Gender.FEMALE)
         );
 
+        //The difference between a built-in array and an ArrayList in Java, is that the size of an array cannot be modified (if you want to add or remove elements to/from an array, you have to create a new one). While elements can be added and removed from an ArrayList whenever you want. The syntax is also slightly different:
         List<Person> females = new ArrayList<>();
 
         //Imperative Apporach
