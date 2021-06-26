e22054032e64eb8542109081c6c19b89f821032c
Sergio Cazaresmuniz <cazasergio96@gmail.com>
2021-06-25 19:33:57 -0500

    adjusted list

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        //The difference between a built-in array and an ArrayList in Java, is that the size of an array cannot be modified (if you want to add or remove elements to/from an array, you have to create a new one). While elements can be added and removed from an ArrayList whenever you want. The syntax is also slightly different:
+//The List interface provides a way to store the ordered collection.
         List<Person> people = List.of(
                 new Person("John", Person.Gender.MALE),
                 new Person("Maria", Person.Gender.FEMALE),
